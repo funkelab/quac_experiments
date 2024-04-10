2d5a1e7e8af87ac1ebc826543405f67dab370352
Diane Adjavon <adjavond@hhmi.org>
2024-04-10 17:41:21 -0400

    chore: :art: Update retina overlay color
    Makes it clearer that it is "quac-annotators-both" rather than "tp-fp-fn"

--- retina/image_attribution/utils.py
+++ retina/image_attribution/utils.py
@@ -7,7 +7,7 @@ from torchvision import transforms
 def load_label(image_path, label_type, img_size=224):
     label_transform = transforms.Compose(
         [
-            transforms.Resize(img_size), # ), interpolation=Image.NEAREST),
+            transforms.Resize(img_size),  # ), interpolation=Image.NEAREST),
             transforms.CenterCrop(img_size),
         ]
     )
@@ -21,7 +21,7 @@ def load_label(image_path, label_type, img_size=224):
     return label
 
 
-def mask_label_overlay(mask, label_image, thresh=0.1):
+def tp_fp_overlay(mask, label_image, thresh=0.1):
     average_mask = mask.mean(axis=0)
     true_positives = np.logical_and(average_mask > thresh, label_image > 0)
     false_negatives = np.logical_and(average_mask < thresh, label_image > 0)
@@ -31,6 +31,31 @@ def mask_label_overlay(mask, label_image, thresh=0.1):
     rgb[false_negatives] = [255, 0, 0]
     return rgb
 
+
+def mask_label_overlay(
+    mask,
+    label_image,
+    thresh=0.1,
+    color_one=(91, 192, 235),
+    color_two=(253, 231, 76),
+    color_both=(155, 197, 61),
+):
+    average_mask = mask.mean(axis=0)
+    true_positives = np.logical_and(average_mask > thresh, label_image > 0)
+    false_negatives = np.logical_and(average_mask < thresh, label_image > 0)
+
+    rgb = np.stack(
+        [   # color_one
+            (c * average_mask).astype(np.uint8) 
+            for c in color_one
+        ],
+        axis=-1,
+    )
+    rgb[true_positives] = color_both
+    rgb[false_negatives] = color_two
+    return rgb
+
+
 def recall(mask, label_image, thresh=0.1):
     average_mask = mask.mean(axis=0)
     true_positives = np.logical_and(average_mask > thresh, label_image > 0)
@@ -39,13 +64,17 @@ def recall(mask, label_image, thresh=0.1):
     recall = true_positives.sum() / max(true_positives.sum() + false_negatives.sum(), 1)
     return recall
 
+
 def precision(mask, label_image, thresh=0.1):
     average_mask = mask.mean(axis=0)
     true_positives = np.logical_and(average_mask > thresh, label_image > 0)
     false_positives = np.logical_and(average_mask > thresh, label_image == 0)
     # precision = TP / (TP + FP)
-    precision = true_positives.sum() / max(true_positives.sum() + false_positives.sum(), 1)
+    precision = true_positives.sum() / max(
+        true_positives.sum() + false_positives.sum(), 1
+    )
     return precision
+
 
 def iou(mask, label_image, thresh=0.1):
     average_mask = mask.mean(axis=0)
